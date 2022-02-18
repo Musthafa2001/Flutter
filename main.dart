ce6b1ea194d8252c1d285299df424cb3f2421520
Musthafa2001 <e1185026@gmail.com>
2022-02-18 10:11:26 +0530

    name

--- main.dart
+++ main.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Musthafa'),
+    );
+  }
+}
