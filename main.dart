7bec388355128af25d54a6327cbaffed985d471b
Musthafa2001 <e1185026@gmail.com>
2022-02-18 10:49:39 +0530

    Myapp class

--- main.dart
+++ main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Musthafa'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Text('This is default Text'),
+      ),
     );
   }
 }
